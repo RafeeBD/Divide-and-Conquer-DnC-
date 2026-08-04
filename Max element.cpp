111bfef30bd0d9ca34f2bdcc7a1c519356f070b9
Mahiduzzaman Rafee <rafeeseak47@gmail.com>
2026-08-04 11:54:46 +0600

    Update Max element.cpp

--- Max element.cpp
+++ Max element.cpp
@@ -23,26 +23,8 @@ int findmax(vector<int> v)
 
 
 }
-int findmin(vector<int> v)
-{
-    if(v.size() == 1)
-    {
-        return v[0];
-    }
-    vector <int> lefthalf,righthalf;
 
-    for(int i = 0;i<v.size()/2;i++)
-    {
-        lefthalf.push_back(v[i]);
-    }
-
-    for(int i = v.size()/2;i<v.size();i++)
-    {
-        righthalf.push_back(v[i]);
-    }
-
-
-    return  min(findmin(lefthalf),findmin(righthalf));
+    
 
 
 }
@@ -56,6 +38,6 @@ int main()
     cin >> v[i];
     }
     cout<< "Max is : "<< findmax(v) << endl;
-    cout<< "Min is : "<< findmin(v) << endl;
+    
 
 }
